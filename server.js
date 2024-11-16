6d0eb5843d5d728b18819124e1263b268cc8ebcc
Dev-Pavithan <pavithanunenthiran29@gmail.com>
2024-11-16 16:30:17 +0530

    vercel2 commit

--- server.js
+++ server.js
@@ -54,7 +54,6 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.error('MongoDB connection error:', err));
-
 // Use routes
 app.use('/user', authRoutes);
 app.use('/login', loginRoutes);
